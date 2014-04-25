f42016b5772cafd8b6fe5c8f453095cdf791a088
zetahernandez <zetahernandez@gmail.com>
2014-04-25 02:16:39 -0300

    logear definición del enviroment

--- src/main/java/org/uda/preciosjustos/configuration/ConfigurableApplicationContextInitializer.java
+++ src/main/java/org/uda/preciosjustos/configuration/ConfigurableApplicationContextInitializer.java
@@ -1,5 +1,7 @@
 package org.uda.preciosjustos.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
@@ -10,11 +12,12 @@ import org.springframework.context.ConfigurableApplicationContext;
  *
  */
 public class ConfigurableApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
+	private static final Logger LOG = LoggerFactory.getLogger(ConfigurableApplicationContextInitializer.class);
+	
 	public void initialize(ConfigurableApplicationContext applicationContext) {
-
+		LOG.debug("initialize enveriment");
 		String profile = System.getProperty("profile");
-
+		LOG.debug("enviroment: " + profile);
 		if (profile == null || profile.equalsIgnoreCase(Profile.DEV.name())) {
 			applicationContext.getEnvironment().setActiveProfiles(Profile.DEV.name().toLowerCase());
 		} else if (profile.equalsIgnoreCase(Profile.PRODUCTION.name())) {
