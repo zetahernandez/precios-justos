d7a88af0457cfa80c9878adeb5dc8864e50d3eb7
zetahernandez <zetahernandez@gmail.com>
2014-04-25 02:04:04 -0300

    configuración de producción

--- src/main/java/org/uda/preciosjustos/configuration/ProductionDatabaseConfig.java
+++ src/main/java/org/uda/preciosjustos/configuration/ProductionDatabaseConfig.java
@@ -1,24 +1,54 @@
 package org.uda.preciosjustos.configuration;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
+import java.util.Properties;
+
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 /**
  * 
  * @author zeta
- *
+ * 
  */
 @Profile("prod")
 @Configuration
+@PropertySource("classpath:prod.properties")
 public class ProductionDatabaseConfig {
-	
-    public DataSource dataSource() throws NamingException {
-		Context ctx = new InitialContext();
-		return (DataSource) ctx.lookup("java:comp/env/jdbc/datasource");
-    }
+	@Autowired
+	private Environment env;
+
+	public DataSource dataSource() throws NamingException {
+		String dburl = System.getProperty("DATABASE_URL");
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(dburl);
+
+		return dataSource;
+	}
+
+	@Bean
+	public Properties hibernateProperties() {
+		return new Properties() {
+			/**
+				 * 
+				 */
+			private static final long serialVersionUID = -1315739295711577049L;
+
+			{
+				setProperty("hibernate.hbm2ddl.auto",
+						env.getProperty("hibernate.hbm2ddl.auto"));
+				setProperty("hibernate.dialect",
+						env.getProperty("hibernate.dialect"));
+				setProperty("hibernate.globally_quoted_identifiers", "true");
+			}
+		};
+	}
 }
