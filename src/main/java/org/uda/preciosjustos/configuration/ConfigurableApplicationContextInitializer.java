e5af33afdb4c57d6b79e4dea28695d7b459a4cb5
Fernando Hernandez <zetahernandez@gmail.com>
2014-05-06 01:49:04 -0300

    logear la elección del profile

--- src/main/java/org/uda/preciosjustos/configuration/ConfigurableApplicationContextInitializer.java
+++ src/main/java/org/uda/preciosjustos/configuration/ConfigurableApplicationContextInitializer.java
@@ -1,5 +1,7 @@
 package org.uda.preciosjustos.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
@@ -11,16 +13,23 @@ import org.springframework.context.ConfigurableApplicationContext;
  */
 public class ConfigurableApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ConfigurableApplicationContextInitializer.class);
+	
 	public void initialize(ConfigurableApplicationContext applicationContext) {
 
 		String profile = System.getenv("profile");
-
+		
+		LOG.debug("Read the enviroment variable profile with the value: " + profile);
+		
 		if (profile == null || profile.equalsIgnoreCase(Profile.DEV.name())) {
 			applicationContext.getEnvironment().setActiveProfiles(Profile.DEV.name().toLowerCase());
+			LOG.debug("Set the active profile as dev");
 		} else if (profile.equalsIgnoreCase(Profile.PROD.name())) {
 			applicationContext.getEnvironment().setActiveProfiles(Profile.PROD.name().toLowerCase());
+			LOG.debug("Set the active profile as prod");
 		} else if (profile.equalsIgnoreCase(Profile.TEST.name())) {
 			applicationContext.getEnvironment().setActiveProfiles(Profile.TEST.name().toLowerCase());
+			LOG.debug("Set the active profile as test");
 		}
 	}
 
