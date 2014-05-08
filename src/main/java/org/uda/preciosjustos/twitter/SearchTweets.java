9df3cf098e3a7bd63077b179f738cea3a3649715
zetahernandez <zetahernandez@gmail.com>
2014-05-08 19:29:58 -0300

    usar postconstruct

--- src/main/java/org/uda/preciosjustos/twitter/SearchTweets.java
+++ src/main/java/org/uda/preciosjustos/twitter/SearchTweets.java
@@ -1,5 +1,7 @@
 package org.uda.preciosjustos.twitter;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,20 +11,20 @@ import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 
 @Component
-public class SearchTweets implements InitializingBean {
+public class SearchTweets{
 
 	private TwitterStream twitterStream;
 	
 	@Autowired
 	private TwitterListener twitterListener;
 	
-	@Override
+	@PostConstruct
 	public void afterPropertiesSet() throws Exception {
 		twitterStream = new TwitterStreamFactory().getInstance();
 	    twitterStream.addListener(twitterListener);
 	    // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
 	    FilterQuery filterQuery = new FilterQuery();
-	    filterQuery.track(new String[] {"preciosjustos"});
+	    filterQuery.track(new String[] {"preguntados"});
 	    twitterStream.filter(filterQuery);
 	}
 
